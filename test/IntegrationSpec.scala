2dac0826cd98fe0ff20af18395f751ed13a55ab6
Tudor Palanga <tudor.palanga@gmail.com>
2014-11-17 12:51:52 +0100

    Add browser test for the websocket connection

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,30 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
+import java.util.concurrent.TimeUnit
 
+import org.fluentlenium.core.filter.Filter
+import org.junit.runner._
+import org.specs2.runner._
 import play.api.test._
-import play.api.test.Helpers._
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpecification {
 
   "Application" should {
 
     "work from within a browser" in new WithBrowser(FIREFOX.newInstance()) {
-
       browser.goTo("http://localhost:" + port)
-
       browser.pageSource must contain("Akka Chat")
     }
+
+    "connect websocket" in new WithBrowser(FIREFOX.newInstance()) {
+      browser.goTo("http://localhost:" + port)
+      browser.waitUntil(10, TimeUnit.SECONDS) {
+        browser.find("#wsStatus", new Filter("connection", "open")).size() > 0
+      }
+    }
+
   }
 }
